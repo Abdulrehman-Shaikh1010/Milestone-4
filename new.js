3619a9ce28615f0b8f0f20cbab7734e72210aaea
Abdulrehman <you@example.com>
2024-12-15 11:25:46 +0500

    Milestone 4

--- new.js
+++ new.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var experience = document.getElementById('experience').value;
         var skills = document.getElementById('skills').value;
         if (name && email && phone && education && experience && skills) {
-            resumeContent.innerHTML = "\n                <p><strong>Name:</strong> ".concat(name, "</p>\n                <p><strong>Email:</strong> ").concat(email, "</p>\n                <p><strong>Phone:</strong> ").concat(phone, "</p>\n                <p><strong>Education:</strong> ").concat(education, "</p>\n                <p><strong>Experience:</strong> ").concat(experience, "</p>\n                <p><strong>Skills:</strong> ").concat(skills, "</p>\n            ");
+            resumeContent.innerHTML = "\n            <h2>Editable Reasume</h2>\n                <p><strong>Name:</strong> ".concat(name, "</p>\n                <p><strong>Email:</strong> ").concat(email, "</p>\n                <p><strong>Phone:</strong> ").concat(phone, "</p>\n                <p><strong>Education:</strong> ").concat(education, "</p>\n                <p><strong>Experience:</strong> ").concat(experience, "</p>\n                <p><strong>Skills:</strong> ").concat(skills, "</p>\n            ");
             resumeOutput.style.display = 'block';
         }
         else {
